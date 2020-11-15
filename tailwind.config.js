9dd4086832189c01da03612e30ae08cd3eb7fb6e
Ludvik <luddmachine@gmail.com>
2020-11-15 04:06:22 +0100

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'src/**/*.html',
+  ],
   theme: {
     extend: {
       fontFamily: {
